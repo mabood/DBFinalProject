71cda2e4127146aa4f7344932f3afaa9f458f17e
mabood <mgabood8@gmail.com>
2017-03-07 13:17:34 -0800

    hewwo worwd!

---
+++ src/guimain/Controller.java
@@ -0,0 +1,12 @@
+package guimain;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Label helloWorld;
+
+    public void sayHelloWorld(ActionEvent actionEvent) {
+        helloWorld.setText("Hewwo Worwd!");
+    }
+}

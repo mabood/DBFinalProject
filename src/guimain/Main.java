66cd7b203a9cab0bb89ec9808a2bd21e9d4d0da9
Chris Yerina <cyerina@gmail.com>
2017-03-14 21:01:22 -0700

    Basic Tab Setup

--- src/guimain/Main.java
+++ src/guimain/Main.java
@@ -2,18 +2,54 @@ package guimain;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+    Stage window;
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("guimain.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+
+        window = primaryStage;
+
+        //Example
+        window.setTitle("Beer DB");
+        Group root = new Group();
+        Scene scene = new Scene(root, 400, 250, Color.WHITE);
+        TabPane tabPane = new TabPane();
+        BorderPane borderPane = new BorderPane();
+        for (int i = 0; i < 5; i++) {
+            Tab tab = new Tab();
+            tab.setGraphic(new Circle(0, 0, 10));
+            HBox hbox = new HBox();
+            hbox.getChildren().add(new Label("Tab" + i));
+            hbox.setAlignment(Pos.CENTER);
+            tab.setContent(hbox);
+            tabPane.getTabs().add(tab);
+        }
+        // bind to take available space
+        borderPane.prefHeightProperty().bind(scene.heightProperty());
+        borderPane.prefWidthProperty().bind(scene.widthProperty());
+
+        borderPane.setCenter(tabPane);
+        root.getChildren().add(borderPane);
+        window.setScene(scene);
+        window.show();
+        //end
+
     }
 
 

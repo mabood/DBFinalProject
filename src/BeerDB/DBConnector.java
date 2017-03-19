bbec84f57d31788803f8bd25bb99832b0b956edd
Garrett <garrett.heald@gmail.com>
2017-03-18 18:49:21 -0700

    Merge branch 'master' of /Users/garrettwheald/IdeaProjects/DBFinalProject with conflicts.

--- src/BeerDB/DBConnector.java
+++ src/BeerDB/DBConnector.java
@@ -7,7 +7,7 @@ public class DBConnector {
 
     static Connection connect;
 
-    public static void connect() {
+    public Connection getConnection() {
 
         try {
             Class.forName("com.mysql.jdbc.Driver");
@@ -19,30 +19,28 @@ public class DBConnector {
         catch (Exception e) {
             e.printStackTrace();
         }
+
+        return connect;
     }
 
-
+/*
     //change return types to ResultSets
-    /*
     public static ResultSet insertTuple(SQLGenerator obj) {
         String statement = obj.generateInsertStatement();
         //execute statement
     }
-*/
+
     // remove tuple
 
     // update tuple
-/*
+
     public static ResultSet queryTable(SQLGenerator obj) {
         String statement = obj.generateGetTableStatement();
         //execute statement
     }
-   */
 
-/*
     public static ResultSet queryTuple(SQLGenerator obj) {
         String statement = obj.generateGetTupleStatement();
     }
-    */
-
+*/
 }

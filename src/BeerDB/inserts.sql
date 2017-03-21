5a114c626da8689cd27bb450173ed5a803cd34d5
Chris Yerina <cyerina@gmail.com>
2017-03-20 21:03:36 -0700

    Updated the schema to cascade on update and delete

---
+++ src/BeerDB/inserts.sql
@@ -0,0 +1,39 @@
+INSERT
+INTO Beer (beerName, breweryName, beerABV, beerIBU, imgURL)
+VALUES ('805 Blonde Ale', 'Firestone', 4.7, 0, 'https://lh6.ggpht.com/iVXdxfjnW2uQlxDumPYc8qLfJmIvhpkRm2cL60ETP3Savug2WkCnI7RloUFM73Af4Iv970cK-4HgE2WVRay1ZtlS2QZV=s750');
+
+INSERT
+INTO Beer (beerName, breweryName, beerABV, beerIBU, imgURL)
+VALUES ('Tap It IPA', 'Tap It Brewing Co.', 7, 0, 'http://www.tapitbrewing.com/wp-content/uploads/2015/11/TAP12oz_IPA_155x375.png');
+
+INSERT
+INTO Beer (beerName, breweryName, beerABV, beerIBU, imgURL)
+VALUES ('Luponic Distortion 005', 'Firestone', 5.9, 0, 'http://www.firestonebeer.com/images/responsive-image.php?LD001_halfpage.jpg');
+
+INSERT
+INTO Beer (beerName, breweryName, beerABV, beerIBU, imgURL)
+VALUES ('Big Sur Double IPA', 'Barrelhouse', 9.6, 0, 'http://www.holidaywinecellar.com/images/Product/large/37167.jpg');
+
+INSERT
+INTO Beer (beerName, breweryName, beerABV, beerIBU, imgURL)
+VALUES ('Tecate', 'Tecate', 4, 0, null);
+
+insert
+into Bar (barName, barLocation)
+VALUES ('Milestone Tavern', 'Foothill Blvd');
+
+insert
+into Brewery (breweryName, breweryLocation, imgURL)
+VALUES ('Barrelhouse', 'Paso Robles, CA', 'http://storage.googleapis.com/americangeneralmedia/cork-dorks/wp-content/uploads/2016/02/19193137/barrel-House.jpg');
+
+insert
+into Brewery (breweryName, breweryLocation, imgURL)
+VALUES ('Firestone', 'Paso Robles, CA', 'https://pbs.twimg.com/profile_images/378800000544558545/71b57bbc085f74413d3a1046b84eaa31_400x400.jpeg');
+
+insert
+into Brewery (breweryName, breweryLocation, imgURL)
+VALUES ('Tap It Brewing Co.', 'San Luis Obispo, CA', 'https://pbs.twimg.com/profile_images/776481581691133952/Nt-GOUWm.jpg');
+
+insert
+into Brewery (breweryName, breweryLocation, imgURL)
+VALUES ('Tecate', 'Mexico', null);
--- src/BeerDB/schema.sql
+++ src/BeerDB/schema.sql
@@ -1,17 +1,22 @@
-CREATE TABLE Brewery (breweryName VARCHAR(100) PRIMARY KEY ,
-                      breweryLocation VARCHAR(100),
-                      imgURL VARCHAR(300));
+CREATE TABLE Brewery (
+    breweryName VARCHAR(100) PRIMARY KEY,
+    breweryLocation VARCHAR(100),
+    imgURL VARCHAR(300));
 
-CREATE TABLE Beer (beerID INT PRIMARY KEY AUTO_INCREMENT,
-                   beerName VARCHAR(100),
-                   breweryName VARCHAR(100),
-                   beerABV REAL,
-                   beerIBU INT,
-                   imgURL VARCHAR(300),
-  FOREIGN KEY (breweryName) REFERENCES Brewery(breweryName));
+CREATE TABLE Beer (
+    beerID INT PRIMARY KEY AUTO_INCREMENT,
+    beerName VARCHAR(100),
+    breweryName VARCHAR(100),
+    beerABV REAL,
+    beerIBU INT,
+    imgURL VARCHAR(300),
+    FOREIGN KEY (breweryName) REFERENCES Brewery(breweryName)
+    ON UPDATE cascade
+    ON DELETE cascade);
 
-CREATE TABLE Bar (barID INT PRIMARY KEY AUTO_INCREMENT,
-                  barName VARCHAR(100),
-                  barLocation VARCHAR(100));
+CREATE TABLE Bar (
+    barID INT PRIMARY KEY AUTO_INCREMENT,
+    barName VARCHAR(100),
+    barLocation VARCHAR(100));
 
 
